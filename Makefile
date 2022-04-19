cb0fcd336a3ae9c6d6a9fd19dbe7755da401b0b1
pinnertw <wayne567tw@yahoo.com.tw>
2022-04-19 22:48:11 +0200

    Change data structure

--- Makefile
+++ Makefile
@@ -1,24 +1,24 @@
 SRC_DIR=src
 OBJ_DIR=obj
+INCLUDE=include
 
 cc=g++
-CFLAGS=-O3
+CFLAGS=-O3 -I$(INCLUDE)
 OPENMPFLAGS=-fopenmp
 
-SRC=$(SRC_DIR)/generator.cpp
-OBJ=$(OBJ_DIR)/generator.o
-
-
-all: $(OBJ_DIR) test #generator
+all: $(OBJ_DIR) params test
 
 $(OBJ_DIR):
 	mkdir $(OBJ_DIR)
 
+params: $(SRC_DIR)/params.cpp
+	$(cc) $(CFLAGS) -DPRINT $< -c -o obj/params.o
+
 generator : $(SRC_DIR)/generator.cpp
-	$(cc) $(CFLAGS) $< -o test.out
+	$(cc) $(CFLAGS) $< $(OBJ_DIR)/params.o -o test.out
 
 test : $(SRC_DIR)/generator.cpp
-	$(cc) $(CFLAGS) $< -DPRINT -o test.out
+	$(cc) $(CFLAGS) -DPRINT $< $(OBJ_DIR)/params.o -o test.out
 
 clean:
 	rm -f $(OBJ)
--- src/generator.cpp
+++ src/generator.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
+#include "params.h"
 using namespace std;
-
 
 #define vvi vector<vector<int>>
 #define vi vector<int>
@@ -9,202 +9,7 @@ using namespace std;
 #define vb vector<bool>
 #define d double
 
-void test(){
-    cout << 0 << endl;
-}
-
-class params
-{
-    private:
-        int M;
-        int R;
-        int type;
-        vi refiners;
-        vd weights;
-        double w_tilde;
-        double alpha;
-        double beta;
-
-        // DP usage in ram
-        vb dp_exist;
-        vd dp_exponant_array;
-
-
-    public:
-        params(int M_, int R_, double alpha_, double beta_){
-            // Init params with M as root, and level R
-            // Then get the refiners with n_j = M^j;
-            M = M_;
-            R = R_;
-            alpha = alpha_;
-            beta = beta_;
-            // Refiners
-            init_refiners();
-            // Weights
-            dp_exist = vb(R, false);
-            dp_exponant_array = vd(R, 0);
-            weights = vd(R, 1.);
-            init_weights();
-
-            print_params();
-        }
-
-        void print_params(){
-#if PRINT1
-            cout << "M, R, alpha, beta"<<endl;
-            cout << M << ", " << R << ", " << alpha << ", " << beta << endl;
-#endif
-        }
-
-        double dp_exponant(int i){
-            if (dp_exist[i]) return dp_exponant_array[i];
-            else{
-                dp_exponant_array[i] = pow(refiners[i], alpha);
-                dp_exist[i] = true;
-                return dp_exponant_array[i];
-            }
-        }
-
-        void init_refiners(){
-            refiners = vi(R);
-            refiners[0] = 1;
-            for (auto i = 1; i < R; i++){
-                refiners[i] = refiners[i-1] * M;
-            }
-#if PRINT1
-            cout << "Print refiners coefficients" << endl;
-            for (auto i = 0; i < R; i++){
-                cout << refiners[i] << endl;
-            }
-#endif
-        }
-
-        void init_weights(){
-            for (auto i = 0; i < R; i++){
-                if ( (R - (i + 1)) % 2 != 0){
-                    weights[i] = -1;
-                    w_tilde = 1;
-                }
-                else{
-                    w_tilde = -1;
-                }
-                double numerator = pow(refiners[i], alpha * (R - 1));
-                double denominator = 1;
-                for (auto j = 0; j < i; j++){
-                    denominator *= dp_exponant(i) - dp_exponant(j);
-                }
-                for (auto j = i + 1; j < R; j++){
-                    denominator *= dp_exponant(j) - dp_exponant(i);
-                }
-                weights[i] *= numerator / denominator;
-                w_tilde /= dp_exponant(i);
-            }
-#if PRINT1
-            cout << "Print weight coefficients" << endl;
-            for (auto i = 0; i < R; i++){
-                cout << weights[i] << ' ';
-            }
-            cout << endl;
-            cout << "w_tilde = " << w_tilde << endl;
-#endif
-        }
-};
-
-class euler_schema
-{
-    private:
-        double step_size;
-        double sqrt_step_size;
-        int total_step;
-        double X0;
-        function<double(double &)> b;
-        function<double(double &)> sigma;
-        vd random_normal;
-    public:
-        euler_schema(double step_size_, int total_step_, double X0_, function<double(double &)> b_, function<double(double &)> sigma_){
-            step_size = step_size_;
-            sqrt_step_size = sqrt(step_size);
-            total_step = total_step_;
-            X0 = X0_;
-            b = b_;
-            sigma = sigma_;
-        }
-
-        void get_normal_distribution(int N){
-            mt19937_64 generator;
-            auto seed = chrono::system_clock::now().time_since_epoch().count();
-            random_normal = vd(N*total_step, 0.);
-            normal_distribution<double> dist(0., 1.);
-            for (auto i=0; i < N*total_step; i++){
-                random_normal[i] = dist(generator);
-            }
-        }
-
-        vvd simulations(int N){
-            get_normal_distribution(N);
-            vvd results = vvd(N, vd(total_step + 1, 0.));
-            for (auto i=0; i < N; i++){
-                results[i][0] = X0;
-                for (auto j = 1; j <= total_step; j++){
-                    results[i][j] = results[i][j-1] + step_size * b(results[i][j-1]) + sqrt_step_size * sigma(results[i][j-1]) * random_normal[i * total_step + j];
-                }
-            }
-#if PRINT1
-            for (auto i=0; i < N; i++){
-                for (auto j=0; j < N; j++){
-                    cout << results[i][j] << ' ';
-                }
-                cout << endl;
-            }
-#endif
-            return results;
-        }
-};
-
-class nested_monte_carlo
-{
-    private:
-        double s0;
-        double r;
-        double sigma;
-        double K1;
-        double K2;
-        double T1;
-        double T2;
-        // Normal dist.
-        vd random_normal;
-    public:
-        nested_monte_carlo(double s0_, double r_, double sigma_, double K1_, double K2_, double T1_, double T2_){
-            s0 = s0_;
-            r = r_;
-            sigma = sigma_;
-            K1 = K1_;
-            K2 = K2_;
-            T1 = T1_;
-            T2 = T2_;
-        }
-        void get_normal_distribution(int N){
-            mt19937_64 generator;
-            auto seed = chrono::system_clock::now().time_since_epoch().count();
-            random_normal = vd(N*2, 0.);
-            normal_distribution<double> dist(0., 1.);
-            for (auto i=0; i < N*2; i++){
-                random_normal[i] = dist(generator);
-            }
-        }
-
-        void simulations(int N){
-            get_normal_distribution(N);
-            vvd results = vvd(N, vd(2, 0.));
-            for (auto i=0; i < N; i++){
-                // TODO: Check how many simulation per one S_T1
-                results[i][0] = s0 * exp((r - 0.5 * sigma * sigma) * T1 + sigma * sqrt(T1) * random_normal[2 * i]);
-                results[i][1] = results[i][0] * exp((r - 0.5 * sigma * sigma) * (T2 - T1) + sigma * sqrt(T2 - T1) * random_normal[2 * i + 1]);
-            }
-        }
-};
-
-class geometric_brownian_payoff
+class estimator
 {
     private:
         double x0;
@@ -223,7 +28,7 @@ class geometric_brownian_payoff
         // TODO
         // Real value, Variance, Complexity, Cost, auto-tuning
     public:
-        geometric_brownian_payoff(int type_, double x0_, double r_, double sigma_, double K_, double T_, 
+        estimator(int type_, double x0_, double r_, double sigma_, double K_, double T_, 
                 double extra_param_ = 1.1, double extra_param2_ = 2.2){
             x0 = x0_;
             r = r_;
@@ -290,29 +95,19 @@ class geometric_brownian_payoff
         }
 };
 
-class estimators
-{
-    private:
-        double h;
-    public:
-        estimators(double h_){
-            h = h_;
-        }
-};
-
-
 int main(int argc, char ** argv)
 {
     params param(5, 6, 0.5, 1.);
-    estimators est(10);
+    param.init_weights();
     function<double(double&)> b = [](double a) {return a * 5.;};
     function<double(double&)> sigma = [](double a) {return 5.;};
     euler_schema schema = euler_schema(0.01, 10, 80, b, sigma);
     schema.simulations(10);
-    geometric_brownian_payoff(1, 100, 0.06, 0.4, 80, 1);
-    geometric_brownian_payoff(1, 100, 0.06, 0.4, 80, 2);
-    geometric_brownian_payoff(1, 100, 0.06, 0.4, 80, 3);
-    geometric_brownian_payoff(1, 100, 0.06, 0.4, 80, 4);
+    estimator(1, 100, 0.06, 0.4, 80, 1);
+    estimator(1, 100, 0.06, 0.4, 80, 2);
+    estimator(1, 100, 0.06, 0.4, 80, 3);
+    estimator(1, 100, 0.06, 0.4, 80, 4);
+    nested_monte_carlo(100., 0.04, 0.3, 10., 20., 1/6, 1/2, 5, 10).simulations(50);
     test();
     return 0;
 }
